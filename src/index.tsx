51df63956c4de171ba256cb3bb3fe11fa9892eca
Hiroto Sasaki(s1270157) <wmid23lim@gmail.com>
2022-08-12 19:47:25 +0900

    Very short commit to restart vercel app

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'; 
 import App from './App';
 //import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
