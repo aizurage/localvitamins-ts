a431b2acd9dd644ce7be134dbaf2f28214681c5c
Hiroto Sasaki(s1270157) <wmid23lim@gmail.com>
2022-09-24 12:53:54 +0900

    Checking logout function was added to prevent user from logging out unexpectively.

--- src/header.jsx
+++ src/header.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
-import { Burger, Button, Drawer, Group } from "@mantine/core";
+import { Burger, Button, Dialog, Drawer, Group } from "@mantine/core";
 import "./header.css";
 import { Link, useNavigate } from 'react-router-dom';
 import { supabase }  from './supabaseClient';
 
 export function Header()
 {
+    const [ dialogopen, setDialogopen ] = useState(false);
+
     const navigate = useNavigate();
 
     const log_out = async() => {
@@ -55,13 +57,26 @@ export function Header()
                                 <li onClick={() => { navigate("/home/serviceTerms", false) }}>利用規約(個人情報の取り扱いについて)</li>
                                 <li onClick={() => { navigate("/home/guide") }}>使い方ガイド</li>
                                 <li onClick={() => { navigate("/home/withdrawal") }}>解約方法</li>
-                                <li onClick={log_out}>ログアウト</li>
+                                <li onClick={() => { setDialogopen(true) }}>ログアウト</li>
                             </ul>
                         </nav>
                         <p>お問い合わせはこちら<br/> wmid23lim@gmail.com  までお願いします。</p>
                     </>
                 }
             </Drawer>
+            <Dialog
+                opened={dialogopen}
+                onClose={ () => {setDialogopen(false)}}
+                position={{ top: 350, left: 200 }}
+            >
+                <p>ログアウトします。よろしいですか？</p>
+                <Group>
+                    {<>
+                        <Button color="red" onClick={ () => { log_out();}}> はい </Button>
+                        <Button color="blue" onClick={ () => {setDialogopen(false)} }> いいえ </Button>
+                    </>}
+                </Group>
+            </Dialog>
         </>
     );
 }
