804509923ff368399d3f6f19f27463e240dc7eae
Peregrine Falcon <69111368+hiro-creater@users.noreply.github.com>
2023-07-10 23:14:40 +0900

    Alert was removed to reduce so many alert of specific events without image pictures

--- src/makingcard.jsx
+++ src/makingcard.jsx
@@ -37,9 +37,11 @@ export function Makingcard(props) {
     } catch (error) {
       console.log('Error downloading image')
       console.log(error.error_description || error.message)
-      alert(
-        '写真のダウンロードに失敗した、もしくは写真がないお手伝いがありました。アプリの動作に影響は無いので、タブを閉じてください。',
-      )
+      // 写真が用意できないケースが考えられるため、一覧を表示するたびに「ダウンロードに失敗した」と表示するのはしつこい。
+      // ストレスになる懸念があるので、アラートはなし。
+      // alert(
+      //  '写真のダウンロードに失敗した、もしくは写真がないお手伝いがありました。アプリの動作に影響は無いので、タブを閉じてください。',
+      // )
     }
   }
 
