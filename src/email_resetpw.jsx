bb0613a9b65a7c52d73663958936163ab5e727b8
Hiroto Sasaki(s1270157) <wmid23lim@gmail.com>
2023-07-10 13:04:01 +0900

    Operation team was changed from eiwa distrinct to charity group Miraikuru.

--- src/email_resetpw.jsx
+++ src/email_resetpw.jsx
@@ -4,68 +4,86 @@ import { useState } from 'react'
 import { At } from 'tabler-icons-react'
 import { useForm } from '@mantine/form'
 
-export default function EmailResetPassword(){
-    const [submitted, setSubmitted] = useState(false)
-    const form = useForm({
-        initialValues: {
-            email: '',
-        },
+export default function EmailResetPassword() {
+  const [submitted, setSubmitted] = useState(false)
+  const form = useForm({
+    initialValues: {
+      email: '',
+    },
 
-        validate: {
-            email: (value) => (/^\S+@\S+$/.test(value) ? null : 'メールアドレスが正しくありません'),
-        },
-    })
+    validate: {
+      email: (value) =>
+        /^\S+@\S+$/.test(value) ? null : 'メールアドレスが正しくありません',
+    },
+  })
 
-    const submit = async (values) => {
-        try {
-            const { error:sendingEmailError } = await supabase.auth.api.resetPasswordForEmail(values.email, {
-                // 送信メールに埋め込まれるリンクのリダイレクト先のURL
-                // reset-passwordへ遷移する
-                redirectTo: 'https://localvitamins-ts.vercel.app/resetpassword',
-            })
-            if (sendingEmailError) throw sendingEmailError
-        } catch (error) {
-            console.log("Sending email for reset password failed")
-            console.log(error.message)
-            alert("メール送信に失敗しました。運営チーム（eiwachiku.c@gmail.com）にお問い合わせください。")
-        }
+  const submit = async (values) => {
+    try {
+      const { error: sendingEmailError } =
+        await supabase.auth.api.resetPasswordForEmail(values.email, {
+          // 送信メールに埋め込まれるリンクのリダイレクト先のURL
+          // reset-passwordへ遷移する
+          redirectTo: 'https://localvitamins-ts.vercel.app/resetpassword',
+        })
+      if (sendingEmailError) throw sendingEmailError
+    } catch (error) {
+      console.log('Sending email for reset password failed')
+      console.log(error.message)
+      alert(
+        'メール送信に失敗しました。運営チーム（miraikuru0512@gmail.com）にお問い合わせください。',
+      )
     }
+  }
 
-    return(
-        <Center>
-            <form onSubmit={form.onSubmit(submit)}>
-                <h1>パスワードの再設定</h1>
-                <p>アカウントに登録したメールアドレスを入力してください。</p>
-                <Input
-                    required
-                    label="メールアドレス"
-                    placeholder="your@email.com"
-                    icon={<At />}
-                    margin="center"
-                    {...form.getInputProps('email')}
-                />
-                <Space h="xl" />
-                <Group position="center" mt="md">
-                    <Button
-                        color='green'
-                        type="submit"
-                        onClick={() => {setSubmitted(true)}}
-                    >
-                        提出する
-                    </Button>
-                </Group>
-                {!submitted ? "" :
-                    <div>
-                        <h2 className='email_resetpw'>注意！</h2>
-                        <p>入力したメールアドレスの、メールボックスを確認してください。<br /> もしメールが来ていない場合は、 次の項目を確認してみてください。</p>
-                        <ul>
-                            <li>登録したメールアドレスとは異なるメールアドレスを入力していないか。</li>
-                            <li>スペルミスをしていないか。</li>
-                        </ul>
-                        <p><font color='red'>確認していただき、それでも解決しない場合は運営チーム（eiwachiku.c@gmail.com）にお問い合わせください。</font></p>
-                    </div>
-                }
-            </form>
-        </Center>
-    )
+  return (
+    <Center>
+      <form onSubmit={form.onSubmit(submit)}>
+        <h1>パスワードの再設定</h1>
+        <p>アカウントに登録したメールアドレスを入力してください。</p>
+        <Input
+          required
+          label="メールアドレス"
+          placeholder="your@email.com"
+          icon={<At />}
+          margin="center"
+          {...form.getInputProps('email')}
+        />
+        <Space h="xl" />
+        <Group position="center" mt="md">
+          <Button
+            color="green"
+            type="submit"
+            onClick={() => {
+              setSubmitted(true)
+            }}
+          >
+            提出する
+          </Button>
+        </Group>
+        {!submitted ? (
+          ''
+        ) : (
+          <div>
+            <h2 className="email_resetpw">注意！</h2>
+            <p>
+              入力したメールアドレスの、メールボックスを確認してください。
+              <br /> もしメールが来ていない場合は、
+              次の項目を確認してみてください。
+            </p>
+            <ul>
+              <li>
+                登録したメールアドレスとは異なるメールアドレスを入力していないか。
+              </li>
+              <li>スペルミスをしていないか。</li>
+            </ul>
+            <p>
+              <font color="red">
+                確認していただき、それでも解決しない場合は運営チーム（miraikuru0512@gmail.com）にお問い合わせください。
+              </font>
+            </p>
+          </div>
+        )}
+      </form>
+    </Center>
+  )
 }
--- src/login.jsx
+++ src/login.jsx
@@ -37,7 +37,7 @@ export default function Login() {
       console.log('Log in failed')
       console.log(error.error_description || error.message)
       alert(
-        'ログイン処理に失敗しました。メールアドレスまたはパスワードに間違いがないかどうか確認してください。それでも解決できない場合は、運営チームeiwachiku.c@gmail.comまでお問い合わせください。',
+        'ログイン処理に失敗しました。メールアドレスまたはパスワードに間違いがないかどうか確認してください。それでも解決できない場合は、運営チームmiraikuru0512@gmail.comまでお問い合わせください。',
       )
     } finally {
       setLoading(false)
@@ -74,7 +74,9 @@ export default function Login() {
             <p>
               パスワードを忘れた方は、下のリンクをクリックして、パスワードの再設定をしてください。
             </p>
-            <Link to={'/email_resetpw'}>パスワードを忘れた方は、ここをクリック。</Link>
+            <Link to={'/email_resetpw'}>
+              パスワードを忘れた方は、ここをクリック。
+            </Link>
             <Space h="xl" />
             <Group position="center" mt="md">
               <Button type="submit" color="orange">
