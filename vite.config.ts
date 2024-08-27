17cd2ef759adb09f27de907d432527bf60b26342
Hiroto Sasaki <wmid23lim@gmail.com>
2024-08-27 21:59:11 +0900

    add: vite's configuration was added

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import env from "vite-plugin-env-compatible";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   server: {
     open: '/index.html',
     port: 5173,
   },
+  plugins: [react(), env({ prefix: "VITE", mountedPath: "process.env" })],
 })
