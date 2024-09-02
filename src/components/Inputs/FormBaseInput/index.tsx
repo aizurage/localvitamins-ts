8cfecfbf05e7fce071a5aaedd230a7587de0c36e
Hiroto Sasaki <wmid23lim@gmail.com>
2024-09-02 09:58:23 +0900

    add: Create FormBaseInput

---
+++ src/components/Inputs/FormBaseInput/index.tsx
@@ -0,0 +1,34 @@
+import { TextInput } from "@mantine/core"
+import { FieldValues, UseFormRegister } from "react-hook-form"
+import { FC, ReactNode } from "react"
+import style from "./index.module.css"
+import { Icon } from "tabler-icons-react"
+
+export interface FormBaseInputProps {
+    required: boolean
+    label?: string
+    placeholder: string
+    fieldname: string
+    register: UseFormRegister<FieldValues>
+    icon?: ReactNode
+}
+
+export const FormBaseInput: FC<FormBaseInputProps> = ({
+    required,
+    label,
+    placeholder,
+    fieldname,
+    register,
+    icon
+}) => {
+    return(
+        <TextInput
+            className={style.FormBaseInputBasicStyle}
+            required={required}
+            label={label}
+            placeholder={placeholder}
+            leftSection={icon}
+            {...register(fieldname)}
+        />
+    )
+}
