889b6fd6a48af445b5c46084cbd5548ff8611fcc
Hiroto Sasaki <wmid23lim@gmail.com>
2024-09-02 10:01:25 +0900

    fix: RegisterAccount page was fixed

---
+++ src/features/RegisterAccountPage/controller/state.ts
@@ -0,0 +1,7 @@
+export interface RegisteredAccountProps {
+    email: string;
+    password: string;
+    username: string;
+    firstname: string;
+    familyname: string;
+}
--- src/features/RegisterAccountPage/view/index.tsx
+++ src/features/RegisterAccountPage/view/index.tsx
@@ -1,127 +1,78 @@
-import {
-  Center,
-  TextInput,
-  Button,
-  Group,
-  PasswordInput,
-  Space,
-  LoadingOverlay,
-} from '@mantine/core'
-import { useForm } from '@mantine/form'
-import { supabase } from './supabaseClient'
+import { Center, Button, Group, LoadingOverlay } from '@mantine/core'
 import { useState } from 'react'
+import { FieldValues, useForm } from 'react-hook-form'
+import { FormBaseInput } from '../../../components/Inputs/FormBaseInput'
+import { PasswordInput } from '../../../components/Inputs/PasswordInput'
+import { submitContents } from '../controller/handleSubmit'
+import { RegisteredAccountProps } from '../controller/state'
 
-export default function Register() {
-  const form = useForm({
-    initialValues: {
-      email: '',
-      password: '',
-      username: '',
-      firstname: '',
-      familyname: '',
-    },
-
-    _validate: {
-      email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
-    },
-    get validate() {
-      return this._validate
-    },
-    set validate(value) {
-      this._validate = value
-    },
-  })
-
+export function RegisterAccountPage() {
   const [loading, setLoading] = useState(false)
-  const submit = async (values) => {
-    try {
-      setLoading(true)
-      const { error } = await supabase.auth.signUp(
-        {
-          email: values.email,
-          password: values.password,
-        },
-        {
-          data: {
-            username: values.username,
-            firstname: values.firstname,
-            familyname: values.familyname,
-          },
-        },
-      )
-      if (error) throw error
-      alert(
-        '登録したメールアドレスに、アプリの案内メールが届きます。この画面を閉じて、メールに記載されているリンクをクリックしてください。新しいアプリ画面が表示されます。',
-      )
-    } catch (error) {
-      console.log('Error new account creation')
-      console.log(error.error_description || error.message)
-      alert(
-        'アカウント作成に失敗しました。入力に不備がないか、パスワードの書式に不備がないかどうかご確認ください。それでも解決しない場合は、アプリ運営チームのメールアドレスmiraikuru0512@gmail.comにお問い合わせください。',
-      )
-    } finally {
-      setLoading(false)
+  const { register, handleSubmit } = useForm()
+
+  const _handleSubmit = (data: FieldValues) => {
+    setLoading(true)
+    const _data: RegisteredAccountProps = {
+      email: data.email,
+      password: data.password,
+      username: data.username,
+      firstname: data.firstname,
+      familyname: data.familyname,
     }
+    submitContents(_data)
   }
 
   return (
-    <>
-      <Center>
-        <h1>アカウント登録</h1>
-      </Center>
-      <Center>
-        <div>
-          <LoadingOverlay visible={loading} />
-          <form onSubmit={form.onSubmit(submit)}>
-            <TextInput
-              label="姓"
-              style={{ width: 500 }}
-              required
-              {...form.getInputProps('familyname')}
-            />
-            <Space h="xl" />
-            <TextInput
-              label="名"
-              style={{ width: 500 }}
-              required
-              {...form.getInputProps('firstname')}
-            />
-            <Space h="xl" />
-            <TextInput
-              required
-              label="ユーザーネーム"
-              placeholder="ユーザーネーム"
-              margin="center"
-              style={{ width: 500 }}
-              {...form.getInputProps('username')}
-            />
-            <Space h="xl" />
-            <TextInput
-              required
-              label="メールアドレス"
-              placeholder="your@email.com"
-              margin="center"
-              style={{ width: 500 }}
-              {...form.getInputProps('email')}
-            />
-            <Space h="xl" />
-            <PasswordInput
-              placeholder="パスワード"
-              label="パスワード"
-              description="パスワードには、文字、数字、そして特殊文字を含めてください。"
-              style={{ width: 500 }}
-              required
-              {...form.getInputProps('password')}
-            />
-            <Space h="xl" />
-            <Group position="center" mt="md">
-              <Button type="submit" color="orange">
-                Submit
-              </Button>
-            </Group>
-          </form>
-        </div>
-      </Center>
-    </>
+    <Center>
+      <h1>アカウント登録</h1>
+      <LoadingOverlay visible={loading} />
+      <form onSubmit={handleSubmit(_handleSubmit)}>
+        <FormBaseInput
+          required={true}
+          label="姓"
+          placeholder="姓"
+          fieldname="familyName"
+          register={register}
+        />
+        <FormBaseInput
+          required={true}
+          label="名"
+          placeholder="名"
+          fieldname="firstName"
+          register={register}
+        />
+        <FormBaseInput
+          required={true}
+          label="ユーザーネーム"
+          placeholder="ユーザーネーム"
+          fieldname="username"
+          register={register}
+        />
+        <FormBaseInput
+          required={true}
+          label="メールアドレス"
+          placeholder="your@email.com"
+          fieldname="email"
+          register={register}
+        />
+        <PasswordInput
+          required={true}
+          label="パスワード"
+          placeholder="パスワード"
+          fieldname="password"
+          description="パスワードには、文字、数字、そして特殊文字を含めてください。"
+          register={register}
+        />
+        <Group 
+          justify="center"
+          align="center" 
+          mt="md"
+        >
+          <Button type="submit" color="orange">
+            Submit
+          </Button>
+        </Group>
+      </form>
+    </Center>
   )
 }
