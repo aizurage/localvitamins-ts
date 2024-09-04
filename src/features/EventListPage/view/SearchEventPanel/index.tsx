aebf81c756cd7ae637dbd5fbe7cf2db9d6f1c267
Hiroto Sasaki <wmid23lim@gmail.com>
2024-09-04 09:30:42 +0900

    add: SearchEventPanel component was almost finished

---
+++ src/features/EventListPage/view/SearchEventPanel/index.tsx
@@ -0,0 +1,16 @@
+import { EventCreationButton } from "./EventCreationButton"
+import { FilteringOwnersEventButton } from "./FilteringOwnersEventButton"
+import { SearchBar } from "./SearchBar"
+import { UnfilteringEventButton } from "./UnfilteringEventButton"
+import styles from "./index.module.css"
+
+export const SearchEventPanel = () => {
+    return(
+        <div className={styles.SearchEventPanel}>
+            <SearchBar />
+            <FilteringOwnersEventButton />
+            <UnfilteringEventButton />
+            <EventCreationButton />
+        </div>
+    )
+}
