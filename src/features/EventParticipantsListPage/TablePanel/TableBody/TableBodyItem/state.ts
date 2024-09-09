176be3f6b3f491d82e32a2fa503921add48f7ddc
Hiroto Sasaki <wmid23lim@gmail.com>
2024-09-09 21:57:56 +0900

    del: unecessary state removed

--- src/features/EventParticipantsListPage/TablePanel/TableBody/TableBodyItem/state.ts
+++
@@ -1,9 +0,0 @@
-export interface Participant{
-    id: number
-    eventID: number
-    eventTitle: string
-    familyname: string
-    firstname: string
-    email: string
-    question?: string
-}
