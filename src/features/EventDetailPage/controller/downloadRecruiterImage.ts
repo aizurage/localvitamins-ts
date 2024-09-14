d7d2e28d18c40120a750aec3b7de0463e48cd0c3
Hiroto Sasaki <wmid23lim@gmail.com>
2024-09-14 13:16:12 +0900

    add: downloadRecruiterImage implemantation finished

---
+++ src/features/EventDetailPage/controller/downloadRecruiterImage.ts
@@ -0,0 +1,13 @@
+import { supabase } from "../../../supabaseClient"
+
+export const downloadRecruiterImage = async (imageUrl: string): Promise<string> => {
+    try {
+      const {data, error} = await supabase.storage.from("recruiter-images").download(imageUrl)
+      if (error) throw new Error("Fetching event recruiter image process in failed")
+      return URL.createObjectURL(data)
+    } catch (error) {
+      console.log('Error downloading event recruiter image')
+      alert("お手伝い募集者の写真ダウンロードに失敗しました。お手伝い一覧画面のメニュー内にある、お問い合わせフォームにてご連絡ください。")
+      return ""
+    }
+}
