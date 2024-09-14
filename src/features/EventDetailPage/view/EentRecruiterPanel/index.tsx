3583b247d0e96ba265ea1fca4a944f35f4640265
Hiroto Sasaki <wmid23lim@gmail.com>
2024-09-14 13:15:07 +0900

    fix: EventRecrutierPanel implementation finished

---
+++ src/features/EventDetailPage/view/EentRecruiterPanel/index.tsx
@@ -0,0 +1,29 @@
+import { FC, useEffect, useState } from "react";
+import { HeadingDescriptionBlock } from "../components/HeadingDescriptionBlock";
+import { EventRecruiter } from "../../../../states";
+import styles from "./index.module.css"
+import { downloadRecruiterImage } from "../../controller/downloadRecruiterImage";
+
+export const EventRecruiterPanel: FC<{eventRecruiter: EventRecruiter}> = ({eventRecruiter}) => {
+    const [recruiterPictureObjectURL, setRecruiterPictureObjectURL] = useState('')
+    useEffect(() => {
+        (async() => {
+            const eventRecruiterPictureObjectUrl = await downloadRecruiterImage(eventRecruiter.imageUrl)
+            setRecruiterPictureObjectURL(eventRecruiterPictureObjectUrl) 
+        })()
+    }, [eventRecruiter.imageUrl])
+
+    return(
+        <div className={styles.eventRecruiterPanelStyle}>
+            <h1>お願いした人</h1>
+            <img
+                className={styles.eventRecruiterImageStyle}
+                src={recruiterPictureObjectURL}
+                alt="お願いした人"
+            />
+            <p>{eventRecruiter.name}さん</p>
+            <HeadingDescriptionBlock heading="自己紹介" description={eventRecruiter.introduction}/>
+            <HeadingDescriptionBlock heading="一言コメント" description={eventRecruiter.comment}/>
+        </div>
+    )
+}
