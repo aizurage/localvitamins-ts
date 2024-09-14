e3487702c5fc0750cf6f90950e366c6015b27077
Hiroto Sasaki <wmid23lim@gmail.com>
2024-09-14 13:23:55 +0900

    add: HeadingDescriptionBlock component was added

---
+++ src/features/EventDetailPage/view/components/HeadingDescriptionBlock/index.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react"
+
+export const HeadingDescriptionBlock: FC<
+    {
+        heading: string,
+        description: string
+    }
+> = ({heading, description}) => {
+    return(
+        <>
+            <h2>{heading}</h2>
+            <p>{description}</p>
+        </>
+    )
+}
