7dbaca8662f4103a14db13e4b8e3356bfc24bcc9
Hiroto Sasaki <wmid23lim@gmail.com>
2024-09-15 16:25:09 +0900

    fix: EventOwnerOptionPanel component and its fodler were fixed(not completed)

--- src/features/EventListPage/components/EventCard/EventOwnerOptionPanel/view/index.tsx
+++ src/features/EventListPage/components/EventCard/EventOwnerOptionPanel/view/index.tsx
@@ -1,33 +1,15 @@
-import { Button } from "@mantine/core"
-import { Link } from "react-router-dom"
+import { FC } from "react"
+import { EventDeletionButton } from "./EventDeletionButton"
+import { EventEditionButton } from "./EventEditionButton"
+import { EventParticipantsDisplayButton } from "./EventParticipantsDisplayButton"
 import styles from "./index.module.css"
 
-export const EventOwnerOptionPanel = () => {
+export const EventOwnerOptionPanel: FC<{eventId: number}> = ({eventId}) => {
     return (
       <div className={styles.ownerOption}>
-        <Button
-          variant="gradient"
-          gradient={{ from: 'orange', to: 'red' }}
-          component={Link}
-          to={`/eventmemberslist/${props.row.id}`}
-        >
-          参加者リスト表示
-        </Button>{' '}
-        <Button
-          color="dark"
-          radius={'xl'}
-          onClick={() => setOpened(true)}
-        >
-          消去
-        </Button>
-        <Button
-          radius={'xl'}
-          className={styles.eventEdit}
-          component={Link}
-          to={`/eventedit/${props.row.id}`}
-        >
-          編集
-        </Button>
+        <EventParticipantsDisplayButton eventId={eventId} />
+        <EventDeletionButton />
+        <EventEditionButton eventId={eventId} />
       </div>
     )
 }
