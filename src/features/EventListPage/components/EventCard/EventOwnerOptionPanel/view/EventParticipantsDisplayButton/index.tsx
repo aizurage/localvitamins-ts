8ab54b188ea4d4240cf56b2087cccab1bd264583
Hiroto Sasaki <wmid23lim@gmail.com>
2024-09-15 16:09:05 +0900

    add: EventParticipantsDisplayButton component and its fodler were added

---
+++ src/features/EventListPage/components/EventCard/EventOwnerOptionPanel/view/EventParticipantsDisplayButton/index.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@mantine/core"
+import { FC } from "react"
+import { Link } from "react-router-dom"
+import style from "./index.module.css"
+
+export const EventParticipantsDisplayButton:FC<{eventId: number}> = ({eventId}) => {
+    return(
+        <Button
+          className={style.button}
+          component={Link}
+          to={`/eventmemberslist/${eventId}`}
+        >
+          参加者リスト表示
+        </Button>
+    )
+}
