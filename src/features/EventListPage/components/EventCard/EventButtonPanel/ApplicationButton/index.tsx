d8348def42695d631a7969d1e195c2522ac4516f
Hiroto Sasaki <wmid23lim@gmail.com>
2024-09-17 22:23:45 +0900

    add: Application Button component and its folder were added

---
+++ src/features/EventListPage/components/EventCard/EventButtonPanel/ApplicationButton/index.tsx
@@ -0,0 +1,28 @@
+import { Button } from "@mantine/core"
+import dayjs, { Dayjs } from "dayjs"
+import { FC } from "react"
+import styles from "./index.module.css"
+
+interface Props {
+    eventId: number
+    title: string
+    date: Dayjs
+    open: (eventId: number, title: string) => void
+}
+
+export const ApplicationButton: FC<Props> = ({
+    eventId,
+    title,
+    date,
+    open
+}) => {
+    return(
+        <Button
+            className={styles.button}
+            onClick={() => open(eventId, title)}
+            disabled={dayjs(new Date).isAfter( date )}
+        >
+            参加する
+        </Button>
+    )
+}
