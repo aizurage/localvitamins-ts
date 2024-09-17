c118e8e1b0dea2cb607ef596ad7b36ed37d66a19
Hiroto Sasaki <wmid23lim@gmail.com>
2024-09-17 23:00:11 +0900

    fix: downloadEventImage function was arranged.

--- src/features/EventListPage/components/EventCard/controller/downloadEventImage.ts
+++ src/features/EventListPage/components/EventCard/controller/downloadEventImage.ts
@@ -1,22 +1,11 @@
-export const downloadEventImage = async (imageUrl) => {
+import { supabase } from "../../../../../supabaseClient"
+
+export const downloadEventImage = async (imageUrl: string): Promise<string> => {
     try {
-      await supabase.storage
-        .from('event-images')
-        .download(imageUrl)
-        .then(
-          (result) =>
-            setEventPictureObjectURL(URL.createObjectURL(result.data)),
-          (error) => {
-            throw error
-          },
-        )
+      const {data, error} = await supabase.storage.from('event-images').download(imageUrl)
+      if (error) throw new Error("Downloading event picture process in failed")
+      return URL.createObjectURL(data)
     } catch (error) {
-      console.log('Error downloading image')
-      console.log(error.error_description || error.message)
-      // 写真が用意できないケースが考えられるため、一覧を表示するたびに「ダウンロードに失敗した」と表示するのはしつこい。
-      // ストレスになる懸念があるので、アラートはなし。
-      // alert(
-      //  '写真のダウンロードに失敗した、もしくは写真がないお手伝いがありました。アプリの動作に影響は無いので、タブを閉じてください。',
-      // )
+      return ""
     }
   }
