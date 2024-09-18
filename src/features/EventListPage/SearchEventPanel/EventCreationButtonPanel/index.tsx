ac24b5eeab168257a4b15eb5fb6114707760df76
Hiroto Sasaki <wmid23lim@gmail.com>
2024-09-18 22:13:45 +0900

    fix: Changed component's name from EventCreationButton to EventCreationButtonPanel

---
+++ src/features/EventListPage/SearchEventPanel/EventCreationButtonPanel/index.tsx
@@ -0,0 +1,17 @@
+import { FC, useState } from "react";
+import { Popover } from "./Popover";
+
+export const EventCreationButtonPanel: FC = () => {
+    return(
+        <Popover
+          content={
+            <p>
+              アカウントを登録し、ログインすることで、あなた自身で「お手伝い」を作ることが出来ます。
+              お手伝い作成、募集を行いたい場合は、ログイン、または新規登録をお願いします。
+            </p>
+          }
+        >
+          <GoToEventMakePageButton />
+        </Popover>
+    )
+}
