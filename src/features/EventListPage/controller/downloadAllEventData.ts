3ffed9dff16dab2c91bec3455f2614bbbcc2a314
Hiroto Sasaki <wmid23lim@gmail.com>
2024-09-18 09:22:02 +0900

    fix: Remove "All" from function name "downloadEventData" and this function was almost completed(still suspicious)

--- src/features/EventListPage/controller/downloadAllEventData.ts
+++
@@ -1,8 +0,0 @@
-export const downloadEventData = async () => {
-    // const today = new Date()
-    const { data } = await supabase
-      .from('EventTable')
-      .select()
-      // .gt('date', dayjs(today))
-    setEvents(data)
-}
