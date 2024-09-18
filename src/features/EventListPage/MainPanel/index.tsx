d541587901e1fa57bf2b6653db2597cf47c953fd
Hiroto Sasaki <wmid23lim@gmail.com>
2024-09-18 09:30:18 +0900

    fix: Main Panel for EventListPage was completed

--- src/features/EventListPage/MainPanel/index.tsx
+++ src/features/EventListPage/MainPanel/index.tsx
@@ -1,17 +1,32 @@
-import { EventCard } from "../../components/EventCard/view"
+import { FC, useEffect, useState } from "react"
+import { EventCard } from "../components/EventCard/view"
 import styles from "./index.module.css"
+import { downloadEventData } from "../controller/downloadEventData"
+import dayjs from "dayjs"
+import { EventPropsForDetailPage } from "../../../states/"
 
-export const MainPanel = () => {
+export const MainPanel: FC = () => {
+    const [events, setEvents] = useState<EventPropsForDetailPage[]>([])
+    useEffect(() => {(async() => {
+        await downloadEventData().then(response => {
+            setEvents(response)
+        }).catch(error => {
+            console.log(error)
+        })
+    })()}, [])
+
     return(
         <div className={styles.eventCards}>
             <nav className={styles.navStyle}>
-                {events.map((row) => (
+                {events.map((event, i) => (
                     <EventCard
-                        row={row}
-                        theme={theme}
-                        open={open}
-                        key={row.id}
-                        setEvents={setEvents}
+                        key={i}
+                        eventId={Number(event.eventID)}
+                        eventPictureUrl={event.event_picture}
+                        title={event.title}
+                        date={dayjs(event.date)}
+                        description={event.content}
+                        plannerUniqueId={event.planner_uniqueID}
                     />
                 ))}
             </nav>
