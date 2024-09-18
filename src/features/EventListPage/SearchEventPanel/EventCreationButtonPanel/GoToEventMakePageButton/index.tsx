2d205b8f5c20f778679335b6c6f08b03e909b457
Hiroto Sasaki <wmid23lim@gmail.com>
2024-09-18 22:12:52 +0900

    add: GoToMakePageButton was added

---
+++ src/features/EventListPage/SearchEventPanel/EventCreationButtonPanel/GoToEventMakePageButton/index.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@mantine/core"
+import { FC } from "react"
+import { useNavigate } from "react-router-dom"
+import styles from "./index.module.css"
+
+export const GoToEventMakePageButton: FC = () => {
+    const navigate = useNavigate()
+    return(
+        <Button
+            className={styles.makeEvent}
+            onClick={() => {
+                navigate("/eventMaker")
+            }}
+        >
+            お手伝い作成
+        </Button>
+    )
+}
