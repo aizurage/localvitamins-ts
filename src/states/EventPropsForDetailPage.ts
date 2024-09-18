3b4136d6a6520e8ebf3884fb01356e0954d5b4e9
Hiroto Sasaki <wmid23lim@gmail.com>
2024-09-18 09:25:09 +0900

    add: New Data type "EventPropsForDetailPage" was added

---
+++ src/states/EventPropsForDetailPage.ts
@@ -0,0 +1,14 @@
+import { Dayjs } from "dayjs"
+
+/**
+ * イベント一覧ページ用のイベント型。\
+ * イベントの型にはたくさんのプロパティがあるため、過剰なプロパティ渡しを防ぐ
+ */
+export interface EventPropsForDetailPage {
+    title: string
+    eventID: number
+    content: string
+    date: Dayjs
+    event_picture: string
+    planner_uniqueID: string
+}
