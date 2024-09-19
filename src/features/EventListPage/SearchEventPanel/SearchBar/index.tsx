a371a4459270f8db94436d6fba62f0a1718d59a6
Hiroto Sasaki <wmid23lim@gmail.com>
2024-09-19 09:52:19 +0900

    add: SearchButton component and its folder were added(Still need to fix)

--- src/features/EventListPage/SearchEventPanel/SearchBar/index.tsx
+++ src/features/EventListPage/SearchEventPanel/SearchBar/index.tsx
@@ -1,23 +1,30 @@
-import { Button, Input } from "@mantine/core"
+import { FieldValues, useForm } from "react-hook-form"
+import { SearchButton } from "./SearchButton"
+import { SearchInput } from "./SearchInput"
 import styles from "./index.module.css"
+import { FC } from "react"
 
-export const SearchBar = () => {
-    return(
-      <>
-        <p>キーワードは最大３つまで入力できます。</p>
-        <p>
-          複数のキーワードで検索をかけるときは、全角スペースで区切ってください。
-        </p>
-        <form className={styles.searchForm} onSubmit={search_keywords_form.onSubmit(search_event)}>
-            <Input
-              placeholder="キーワードを入力"
-              className={styles.searchBar}
-              {...search_keywords_form.getInputProps('keywords')}
-            />
-            <Button className="search_button" type="submit">
-              検索
-            </Button>
-        </form>
-      </>
-    )
+export const SearchBar: FC = () => {
+  const { register, handleSubmit } = useForm()
+  const _handleSubmit = (data: FieldValues) => {
+    // TODO: 全角スペース もしくは 半角スペースで区切る処理が必要
+    submitContents(_data)
+  }
+
+  return(
+    <>
+      <p>キーワードは最大３つまで入力できます。</p>
+      <p>
+        複数のキーワードで検索をかけるときは、全角スペースで区切ってください。
+      </p>
+      <form className={styles.searchForm} onSubmit={handleSubmit(_handleSubmit)}>
+        <SearchInput
+          placeholder="キーワードを入力"
+          fieldname="keyWords"
+          register={register}
+        />
+        <SearchButton />
+      </form>
+    </>
+  )
 }
