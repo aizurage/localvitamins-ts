c7a68c3b26db4bd28e540b27802848e69a113bc7
Hiroto Sasaki <wmid23lim@gmail.com>
2024-09-19 09:40:26 +0900

    add: SearchInput component and its folder were added

---
+++ src/features/EventListPage/SearchEventPanel/SearchBar/SearchInput/index.tsx
@@ -0,0 +1,20 @@
+import { TextInput } from "@mantine/core"
+import { FC } from "react"
+import { UseFormRegister, FieldValues } from "react-hook-form"
+import styles from "./index.module.css"
+
+interface Props {
+    placeholder: string
+    fieldname: string
+    register: UseFormRegister<FieldValues>
+}
+
+export const SearchInput: FC<Props> = ({placeholder, fieldname, register}) => {
+    return(
+        <TextInput
+            className={styles.searchBar}
+            placeholder={placeholder}
+            {...register(fieldname)}
+        />
+    )
+}
