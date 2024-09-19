c50005c211d646043d7b329f4d346ab39f0ab5bf
Hiroto Sasaki <wmid23lim@gmail.com>
2024-09-19 09:31:10 +0900

    del: Unnecessary import statement was removed

--- src/components/Inputs/FormBaseInput/index.tsx
+++ src/components/Inputs/FormBaseInput/index.tsx
@@ -2,7 +2,6 @@ import { TextInput } from "@mantine/core"
 import { FieldValues, UseFormRegister } from "react-hook-form"
 import { FC, ReactNode } from "react"
 import style from "./index.module.css"
-import { Icon } from "tabler-icons-react"
 
 export interface FormBaseInputProps {
     required: boolean
