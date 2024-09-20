b838255c47181dd45cecf74416c48add3639b7da
Hiroto Sasaki <wmid23lim@gmail.com>
2024-09-20 20:46:40 +0900

    add: logout function was extracted as an individual function

---
+++ src/components/Header/controller/logout.ts
@@ -0,0 +1,6 @@
+import { supabase } from "../../../supabaseClient"
+
+export const logout = async (): Promise<void> => {
+    const { error } = await supabase.auth.signOut()
+    if (error) throw new Error("Logout process in failed")
+}
