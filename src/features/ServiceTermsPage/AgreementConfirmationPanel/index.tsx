3278005d171d3a84591e270f7da17d833308c54a
Hiroto Sasaki <wmid23lim@gmail.com>
2024-09-20 18:11:32 +0900

    fix: ServiceTermsPage component and its folder were updated

---
+++ src/features/ServiceTermsPage/AgreementConfirmationPanel/index.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@mantine/core"
+import { FC } from "react"
+import { Link } from "react-router-dom"
+
+export const AgreementConfirmationPanel: FC = () => {
+    return(
+        <>
+            <p>以上の個人情報の使用条件について同意いただける場合は、「同意する」ボタンを押してください。アカウント登録画面が表示されます。</p>
+            <Button
+                color="red"
+                component={Link}
+                to="/register"
+            >同意する</Button>
+        </>
+    )
+}
