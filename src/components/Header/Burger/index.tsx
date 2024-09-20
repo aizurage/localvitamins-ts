1b50dfa9be03f868609bc2f4e4612ce730dbaa2c
Hiroto Sasaki <wmid23lim@gmail.com>
2024-09-20 23:23:40 +0900

    add: Burger for Header component was added

---
+++ src/components/Header/Burger/index.tsx
@@ -0,0 +1,16 @@
+import { FC } from "react"
+import { Burger as _Burger } from "@mantine/core";
+import styles from "./index.module.css"
+
+interface Props {
+    setOpen: (arg0: boolean) => void
+}
+
+export const Burger: FC<Props> = ({setOpen}) => {
+    return(
+        <_Burger
+            onClick={() => setOpen(!open)}
+            className={styles.burger}
+        />
+    )
+}
