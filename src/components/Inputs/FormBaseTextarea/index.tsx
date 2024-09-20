16323339aee76a63fb02ed933a5258399c0bc72d
Hiroto Sasaki <wmid23lim@gmail.com>
2024-09-20 23:57:45 +0900

    add: FormBaseTextarea component were added

---
+++ src/components/Inputs/FormBaseTextarea/index.tsx
@@ -0,0 +1,36 @@
+import { Textarea } from "@mantine/core"
+import { FieldValues, UseFormRegister } from "react-hook-form"
+import { FC, ReactNode } from "react"
+import styles from "./index.module.css"
+
+export interface Props {
+    required: boolean
+    label?: string
+    placeholder: string
+    fieldname: string
+    minRows: number
+    register: UseFormRegister<FieldValues>
+    icon?: ReactNode
+}
+
+export const FormBaseTextarea: FC<Props> = ({
+    required,
+    label,
+    placeholder,
+    fieldname,
+    minRows,
+    register,
+    icon
+}) => {
+    return(
+        <Textarea
+            className={styles.textarea}
+            required={required}
+            label={label}
+            placeholder={placeholder}
+            minRows={minRows}
+            leftSection={icon}
+            {...register(fieldname)}
+        />
+    )
+}
