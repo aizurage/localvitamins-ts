94483ca8d7a2a179437b7e7e7d25854659a64e96
Hiroto Sasaki <wmid23lim@gmail.com>
2024-09-20 23:27:25 +0900

    del: view directory in Layout component was removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
-import '@mantine/core/styles.css'
-import { MantineProvider } from '@mantine/core'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { PAGES, createRoutes } from './routes/routes'
-import { Layout } from './components/Layout/view'
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { PAGES, createRoutes } from "./routes/routes";
+import { Layout } from "./components/Layout";
 
 export default function App() {
-  
   return (
     <BrowserRouter>
       <MantineProvider>
         <Routes>
-          <Route element={<Layout />}>
-            {createRoutes(PAGES)}
-          </Route>
+          <Route element={<Layout />}>{createRoutes(PAGES)}</Route>
         </Routes>
       </MantineProvider>
     </BrowserRouter>
-  )
+  );
 }
--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 import { Outlet } from "react-router-dom";
-import { Header } from "../../Header/view";
+import { Header } from "../Header";
 
 export const Layout: FC = () => {
   return (
