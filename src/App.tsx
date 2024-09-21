0131d5b961712b3850ab62904731b17b60bedb0d
Hiroto Sasaki <wmid23lim@gmail.com>
2024-09-22 07:47:20 +0900

    fix: All Route components were wrapped with react-redux Provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,18 @@ import { MantineProvider } from "@mantine/core";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { PAGES, createRoutes } from "./routes/routes";
 import { Layout } from "./components/Layout";
+import { Provider } from "react-redux";
+import store from "./app/store"
 
 export default function App() {
   return (
     <BrowserRouter>
       <MantineProvider>
-        <Routes>
-          <Route element={<Layout />}>{createRoutes(PAGES)}</Route>
-        </Routes>
+        <Provider store={store}>
+          <Routes>
+            <Route element={<Layout />}>{createRoutes(PAGES)}</Route>
+          </Routes>
+        </Provider>
       </MantineProvider>
     </BrowserRouter>
   );
