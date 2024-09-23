e8b417613fc8165e1a9ab18eeb57537455361636
Hiroto Sasaki <wmid23lim@gmail.com>
2024-09-23 11:34:19 +0900

    add: Deleting event picture and event recruiter picture functions were added

---
+++ src/features/EventMakingPage/controller/deleteEventPicture.ts
@@ -0,0 +1,6 @@
+import { supabase } from "../../../supabaseClient"
+
+export const deleteEventPicture = async (eventPictureUrl: string): Promise<void> => {
+    const {error} = await supabase.storage.from("recruiter-images").remove([eventPictureUrl])
+    if(error) throw new Error("Deleting event picture from database storage in failed")
+}
---
+++ src/features/EventMakingPage/controller/deleteEventRecruiterPicture.ts
@@ -0,0 +1,6 @@
+import { supabase } from "../../../supabaseClient"
+
+export const deleteEventRecruiterPicture = async (recruiterPictureUrl: string) => {
+    const {error} = await supabase.storage.from("recruiter-images").remove([recruiterPictureUrl])
+    if(error) throw new Error("Deleting event recruiter picture from database storage in failed")
+}
