193550ceef22795085fcb4274d3e6cdf9856cedd
Hiroto Sasaki <wmid23lim@gmail.com>
2024-09-23 12:15:27 +0900

    add: EventRecruiterFormPanel component and its related component were added

---
+++ src/features/EventMakingPage/EventRecruiterFormPanel/EventRecruiterPictureInput/index.tsx
@@ -0,0 +1,29 @@
+import { FC } from "react"
+import { useAppDispatch } from "../../../../app/hook"
+import { setEventRecruiterPicture } from "../../../../app/slices/eventRecruiterPictureSlice"
+import styles from "./index.module.css"
+
+interface Props {
+    disabled: boolean
+}
+
+export const EventRecruiterPictureInput: FC<Props> = ({disabled}) => {
+    const dispatch = useAppDispatch()
+    const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const picture = event.target.files?.[0]
+        if (picture) dispatch(setEventRecruiterPicture(picture))
+    }
+
+    return(
+        <>
+            <h4>お手伝いイメージ画像の選択</h4>
+            <input
+                className={styles.imageInput}
+                type="file"
+                accept="image/*"
+                onChange={onChange}
+                disabled={disabled}
+            />
+        </>
+    )
+}
---
+++ src/features/EventMakingPage/EventRecruiterFormPanel/index.tsx
@@ -0,0 +1,38 @@
+import { FC } from "react"
+import { UseFormRegister, FieldValues } from "react-hook-form"
+import { FormBaseInput } from "../../../components/Inputs/FormBaseInput"
+import { EventRecruiterPictureInput } from "./EventRecruiterPictureInput"
+
+interface Props {
+  register: UseFormRegister<FieldValues>
+}
+
+export const EventRecruiterFormPanel: FC<Props> = ({register}) => {
+    return(
+        <>
+            <h1>お手伝い募集者情報入力</h1>
+            <FormBaseInput 
+                required
+                label="募集者の名前"
+                placeholder="お手伝い募集者の名前"
+                fieldname="recruiter_name"
+                register={register}
+            />
+            <FormBaseInput 
+                required
+                label="自己紹介文"
+                placeholder="募集者の自己紹介文"
+                fieldname="recruiter_introduction"
+                register={register}
+            />
+            <FormBaseInput 
+                required
+                label="一言コメント"
+                placeholder="募集者の一言コメント"
+                fieldname="recruiter_comment"
+                register={register}
+            />
+            <EventRecruiterPictureInput disabled={false} />
+        </>
+    )
+}
