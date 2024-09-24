cef0c32e5c9a4367019d48ea8749eac2f80ea923
Hiroto Sasaki <wmid23lim@gmail.com>
2024-09-24 22:49:12 +0900

    fix: EventPanel was updated

--- src/features/EventDetailPage/EventPanel/index.tsx
+++ src/features/EventDetailPage/EventPanel/index.tsx
@@ -1,41 +1,53 @@
 import { FC, useEffect, useState } from "react";
-import type { Event } from "../../../states"
-import styles from "./index.module.css"
-import { HeadingDescriptionBlock } from "../components/HeadingDescriptionBlock";
-import { DateTimeBlock } from "../components/DateTimeBlock";
+import type { Event } from "../../../states";
+import styles from "./index.module.css";
+import { HeadingDescriptionBlock } from "../../../components/HeadingDescriptionBlock";
+import { DateTimeBlock } from "../../../components/DateTimeBlock";
 import { downloadEventImage } from "../controller/downloadEventImage";
 
-export const EventPanel: FC<{event: Event}> = ({event}) => {
-    const [eventPictureObjectURL, setEventPictureObjectURL] = useState('')
-    useEffect(() => {
-        (async() => {
-            const eventPictureObjectUrl = await downloadEventImage(event.imageUrl)
-            setEventPictureObjectURL(eventPictureObjectUrl)
-        })()
-    }, [event.imageUrl])
-    
-    return(
-        <>
-            <img
-                className={styles.eventImageStyle}
-                src={eventPictureObjectURL}
-                alt={event.title}
-            />
-            <h1>{event.title}</h1>
-            <HeadingDescriptionBlock heading="開催場所" description={event.region} />
-            <DateTimeBlock 
-                heading="開催日時" 
-                date={event.date} 
-                from={event.period.from} 
-                to={event.period.to} 
-            />
-            <HeadingDescriptionBlock heading="参加して欲しい人" description={event.target} />
-            <HeadingDescriptionBlock heading="お手伝い内容" description={event.content} />
-            <HeadingDescriptionBlock heading="持ち物" description={event.belongings} />
-            <HeadingDescriptionBlock heading="服装" description={event.clothes} />
-            <HeadingDescriptionBlock heading="お礼" description={event.reward} />
-            <HeadingDescriptionBlock heading="集合場所" description={event.site} />
-            <HeadingDescriptionBlock heading="お問い合わせ先" description={event.inquiry} />
-        </>
-    )
-}
+export const EventPanel: FC<{ event: Event }> = ({ event }) => {
+  const [eventPictureObjectURL, setEventPictureObjectURL] = useState("");
+  useEffect(() => {
+    (async () => {
+      const eventPictureObjectUrl = await downloadEventImage(event.imageUrl);
+      setEventPictureObjectURL(eventPictureObjectUrl);
+    })();
+  }, [event.imageUrl]);
+
+  return (
+    <>
+      <img
+        className={styles.eventImageStyle}
+        src={eventPictureObjectURL}
+        alt={event.title}
+      />
+      <h1>{event.title}</h1>
+      <HeadingDescriptionBlock heading="開催場所" description={event.region} />
+      <DateTimeBlock
+        heading="開催日時"
+        date={event.date}
+        from={event.period.from}
+        to={event.period.to}
+      />
+      <HeadingDescriptionBlock
+        heading="参加して欲しい人"
+        description={event.target}
+      />
+      <HeadingDescriptionBlock
+        heading="お手伝い内容"
+        description={event.content}
+      />
+      <HeadingDescriptionBlock
+        heading="持ち物"
+        description={event.belongings}
+      />
+      <HeadingDescriptionBlock heading="服装" description={event.clothes} />
+      <HeadingDescriptionBlock heading="お礼" description={event.reward} />
+      <HeadingDescriptionBlock heading="集合場所" description={event.site} />
+      <HeadingDescriptionBlock
+        heading="お問い合わせ先"
+        description={event.inquiry}
+      />
+    </>
+  );
+};
