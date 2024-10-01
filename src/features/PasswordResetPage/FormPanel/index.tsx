5790c65d78a8530b8378f68cf220a6c67d16c7c8
Hiroto Sasaki <wmid23lim@gmail.com>
2024-10-01 22:38:35 +0900

    fix: Formatting files in PasswordResetPage

--- src/features/PasswordResetPage/FormPanel/index.tsx
+++ src/features/PasswordResetPage/FormPanel/index.tsx
@@ -7,7 +7,7 @@ import { handleNewPasswordSubmit } from "../controller/handleNewPasswordSubmit"
 import styles from "./index.module.css"
 
 export const FormPanel: FC = () => {
-  const [loading, setLoading] = useState(false)
+  const [ loading, setLoading ] = useState(false)
   const { register, handleSubmit } = useForm()
   const navigate = useNavigate()
 
@@ -15,7 +15,7 @@ export const FormPanel: FC = () => {
     setLoading(true)
     await handleNewPasswordSubmit(values.password)
       .then(() => {
-        alert( 
+        alert(
           `
           「OK」をクリックし、パスワードの再設定が完了するまでこのままお待ちください。
           パスワードの設定が完了したら、ログイン画面に自動で切り替わります。
@@ -35,28 +35,28 @@ export const FormPanel: FC = () => {
   }
 
   return(
-        <form onSubmit={handleSubmit(submit)}>
-          <LoadingOverlay visible={loading} />
-          <FormBaseInput
-              required
-              register={register}
-              label="パスワード"
-              placeholder="新しいパスワード"
-              fieldname="password"
-          />
-          <FormBaseInput
-              required
-              register={register}
-              label="パスワード(確認用)"
-              placeholder="新しいパスワード(確認用)"
-              fieldname="conformedPassword"
-          />
-          <p className={styles.redChar}>
+    <form onSubmit={handleSubmit(submit)}>
+      <LoadingOverlay visible={loading} />
+      <FormBaseInput
+        required
+        register={register}
+        label="パスワード"
+        placeholder="新しいパスワード"
+        fieldname="password"
+      />
+      <FormBaseInput
+        required
+        register={register}
+        label="パスワード(確認用)"
+        placeholder="新しいパスワード(確認用)"
+        fieldname="conformedPassword"
+      />
+      <p className={styles.redChar}>
             パスワードは忘れないようにメモし、大切に保管してください。
-          </p>
-          <Button className={styles.submitButton} type="submit">
+      </p>
+      <Button className={styles.submitButton} type="submit">
             提出する
-          </Button>
-        </form>
-    )
+      </Button>
+    </form>
+  )
 }
--- src/features/PasswordResetPage/controller/handleNewPasswordSubmit.ts
+++ src/features/PasswordResetPage/controller/handleNewPasswordSubmit.ts
@@ -1,10 +1,10 @@
 import { supabase } from "../../../supabaseClient"
 
 export const handleNewPasswordSubmit = async(
-    password: string
+  password: string
 ): Promise<void> => {
-    const { error } = await supabase.auth.updateUser({
-        password: password,
-    })
-    if (error) throw new Error("Password reset process in failed")
+  const { error } = await supabase.auth.updateUser({
+    password,
+  })
+  if (error) throw new Error("Password reset process in failed")
 }
