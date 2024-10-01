40c4406c33ddb010e21e1f35cb9dc708ee83c1d1
Hiroto Sasaki <wmid23lim@gmail.com>
2024-10-01 22:34:21 +0900

    fix: Formatting files in EmailForPasswordResetPage

--- src/features/EmailForPasswordResetPage/controller/handleEmailSubmit.ts
+++ src/features/EmailForPasswordResetPage/controller/handleEmailSubmit.ts
@@ -1,11 +1,11 @@
 import { supabase } from "../../../supabaseClient"
 
 export const handleEmailSubmit = async (email: string): Promise<void> => {
-    const { error } = await supabase.auth.resetPasswordForEmail(
-        email, 
-        {
-            redirectTo: 'https://localvitamins-ts.vercel.app/resetpassword',
-        }
-    )
-    if (error) throw new Error("Sending email for reset password in failed")
+  const { error } = await supabase.auth.resetPasswordForEmail(
+    email,
+    {
+      redirectTo: 'https://localvitamins-ts.vercel.app/resetpassword',
+    }
+  )
+  if (error) throw new Error("Sending email for reset password in failed")
 }
