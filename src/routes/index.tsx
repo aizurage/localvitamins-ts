50b2346638ad455d66ea2101bf11705344d1ea78
Hiroto Sasaki <wmid23lim@gmail.com>
2024-10-01 22:38:06 +0900

    fix: Formatting files in routes directory

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,50 +1,50 @@
-import { Route } from "react-router-dom";
-import { ReactNode } from "react";
-import { EventListPage } from "../features/EventListPage";
-import { EventMakingPage } from "../features/EventMakingPage";
-import { EventDetailPage } from "../features/EventDetailPage";
-import { EventParticipantsListPage } from "../features/EventParticipantsListPage";
-import { ServiceTermsPage } from "../features/ServiceTermsPage";
-import { EventEditPage } from "../features/EventEditPage";
+import { ReactNode } from "react"
+import { Route } from "react-router-dom"
+import { EventDetailPage } from "../features/EventDetailPage"
+import { EventEditPage } from "../features/EventEditPage"
+import { EventListPage } from "../features/EventListPage"
+import { EventMakingPage } from "../features/EventMakingPage"
+import { EventParticipantsListPage } from "../features/EventParticipantsListPage"
+import { ServiceTermsPage } from "../features/ServiceTermsPage"
 
 const PAGES = [
-    {
-        path: "/",
-        element: <EventListPage />,
-    },
-    {
-        path: "/eventMaking",
-        element: <EventMakingPage />,
-    },
-    {
-        path: "/eventDetail/:eventNumber",
-        element: <EventDetailPage />,
-    },
-    {
-        path: "/eventEdit/:eventNumber",
-        element: <EventEditPage />,
-    },
-    {
-        path: "/eventParticipantsList/:eventNumber",
-        element: <EventParticipantsListPage />,
-    },
-    {
-        path: "/serviceTerms",
-        element: <ServiceTermsPage isAgreementNecessary={false} />,
-    },
+  {
+    path: "/",
+    element: <EventListPage />,
+  },
+  {
+    path: "/eventMaking",
+    element: <EventMakingPage />,
+  },
+  {
+    path: "/eventDetail/:eventNumber",
+    element: <EventDetailPage />,
+  },
+  {
+    path: "/eventEdit/:eventNumber",
+    element: <EventEditPage />,
+  },
+  {
+    path: "/eventParticipantsList/:eventNumber",
+    element: <EventParticipantsListPage />,
+  },
+  {
+    path: "/serviceTerms",
+    element: <ServiceTermsPage isAgreementNecessary={false} />,
+  },
 ]
 
 const createRoutes = (
-    pages: readonly {
+  pages: readonly {
         readonly path: string,
         readonly element: ReactNode
     }[]
 ) => {
-    return(
-        pages.map((page, i) => (
-            <Route key={i} path={page.path} element={page.element} />
-        ))
-    )
+  return(
+    pages.map((page, i) => (
+      <Route key={i} path={page.path} element={page.element} />
+    ))
+  )
 }
 
-export {PAGES, createRoutes}
+export { PAGES, createRoutes }
