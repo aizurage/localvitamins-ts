2e862e68cbeed50a586e4dc9b38e57285861bd2d
Hiroto Sasaki <wmid23lim@gmail.com>
2024-10-01 21:37:26 +0900

    fix: Adjustment of import orders for PasswordResetPage

--- src/features/PasswordResetPage/FormPanel/index.tsx
+++ src/features/PasswordResetPage/FormPanel/index.tsx
@@ -1,10 +1,10 @@
 import { FC, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { Button, LoadingOverlay } from "@mantine/core"
 import { FieldValues, useForm } from "react-hook-form"
 import { FormBaseInput } from "../../../components/Inputs/FormBaseInput"
-import { Button, LoadingOverlay } from "@mantine/core"
-import styles from "./index.module.css"
 import { handleNewPasswordSubmit } from "../controller/handleNewPasswordSubmit"
-import { useNavigate } from "react-router-dom"
+import styles from "./index.module.css"
 
 export const FormPanel: FC = () => {
   const [loading, setLoading] = useState(false)
@@ -16,12 +16,18 @@ export const FormPanel: FC = () => {
     await handleNewPasswordSubmit(values.password)
       .then(() => {
         alert( 
-          '「OK」をクリックし、パスワードの再設定が完了するまでこのままお待ちください。パスワードの設定が完了したら、ログイン画面に自動で切り替わります。'
+          `
+          「OK」をクリックし、パスワードの再設定が完了するまでこのままお待ちください。
+          パスワードの設定が完了したら、ログイン画面に自動で切り替わります。
+          `
         )
         navigate('/login')
       }).catch(() => {
         alert(
-          'パスワード再設定に失敗しました。お手伝い一覧画面のメニュー内にある、お問い合わせフォームにてご連絡ください。',
+          `
+          パスワード再設定に失敗しました。
+          お手伝い一覧画面のメニュー内にある、お問い合わせフォームにてご連絡ください。
+          `
         )
       }).finally(() => {
         setLoading(false)
