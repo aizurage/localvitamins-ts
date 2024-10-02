423e11909cefb670f7a66f7c68e2a0b0cfd82899
Hiroto Sasaki <wmid23lim@gmail.com>
2024-10-02 21:14:54 +0900

    fix: SearchEventPanel has been almost completed

--- src/features/EventListPage/SearchEventPanel/index.tsx
+++ src/features/EventListPage/SearchEventPanel/index.tsx
@@ -1,16 +1,37 @@
-import { EventCreationButton } from "./EventCreationButton"
+import { useState } from "react"
+import { useAppSelector } from "../../../app/hook"
+import { downloadEventData } from "../controller/downloadEventData"
+import { downloadUserEventData } from "../controller/downloadUserEventData"
+import { EventCreationButtonPanel } from "./EventCreationButtonPanel"
 import { FilteringOwnersEventButton } from "./FilteringOwnersEventButton"
 import { SearchBar } from "./SearchBar"
 import { UnfilteringEventButton } from "./UnfilteringEventButton"
 import styles from "./index.module.css"
 
 export const SearchEventPanel = () => {
+  const [ isEventFilterd, setIsEventFilterd ] = useState(false)
+  const user = useAppSelector(state => state.user.user)
   return(
     <div className={styles.SearchEventPanel}>
       <SearchBar />
-      <FilteringOwnersEventButton />
-      <UnfilteringEventButton />
-      <EventCreationButton />
+      <div>
+        {
+          isEventFilterd ?
+            <UnfilteringEventButton
+              onClick={() => {
+                setIsEventFilterd(false)
+                downloadUserEventData(user.id)
+              }}/> :
+            <FilteringOwnersEventButton
+              onClick={() => {
+                setIsEventFilterd(true)
+                downloadEventData()
+              }}
+            />
+        }
+      </div>
+
+      <EventCreationButtonPanel />
     </div>
   )
 }
