277c4a2f5a592ced2e344e7a8f808affb222d31d
Hiroto Sasaki <wmid23lim@gmail.com>
2024-10-06 00:37:59 +0900

    fix: functions in controller in EventCard were almost completed

--- src/features/EventListPage/MainPanel/EventCard/controller/deleteEvent.ts
+++ src/features/EventListPage/MainPanel/EventCard/controller/deleteEvent.ts
@@ -5,23 +5,19 @@ import { deleteRecruiterImage } from "./deleteRecruiterImage"
 import { fetchEvent } from "./fetchEvent"
 
 export const deleteEvent = async (eventId: number) => {
-    const {
-      eventPictureUrl: event_picture,
-      eventRecruiterPictureUrl: recruiter_picture
-    } = await fetchEvent(
-      eventId,
-      "event_picture, recruiter_picture"
-    )
-    const { error } = await supabase.from('EventTable').delete().match({ id: eventId })
-    if (error) throw new Error("Deleting event data in failed")
-    deleteEventImage(eventPictureUrl)
-    deleteRecruiterImage(eventRecruiterPictureUrl)
-    deleteEventParticipants(eventId)    
-  } catch (error) {
-    console.log('Event deletion failed')
-    console.log(error.error_description || error.message)
-    alert(
-      'イベントのデータ消去に失敗しました。お手伝い一覧画面のメニュー内にある、お問い合わせフォームにてご連絡ください。',
-    )
-  }
+  const {
+    event_picture: eventPictureUrl,
+    recruiter_picture: eventRecruiterPictureUrl
+  } = await fetchEvent(
+    eventId,
+    "event_picture, recruiter_picture"
+  )
+  const { error } = await supabase
+    .from('EventTable')
+    .delete()
+    .match({ id: eventId })
+  if (error) throw new Error("Deleting event data in failed")
+  deleteEventImage(eventPictureUrl)
+  deleteRecruiterImage(eventRecruiterPictureUrl)
+  deleteEventParticipants(eventId)
 }
--- src/features/EventListPage/MainPanel/EventCard/controller/fetchEvent.ts
+++ src/features/EventListPage/MainPanel/EventCard/controller/fetchEvent.ts
@@ -4,7 +4,7 @@ export const fetchEvent = async (eventId: number, columns: string) => {
   const { data, error } = await supabase
     .from('EventTable')
     .select(columns)
-    .match({ id: eventId })
+    .eq("id", eventId)
   if (error) throw error
   return data
 }
